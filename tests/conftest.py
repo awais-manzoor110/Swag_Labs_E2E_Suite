df8f0e8122be9753d91e32c2a25f2b0dc4c2f7f0
Awais Manzoor <116518958+awais-manzoor110@users.noreply.github.com>
2023-07-14 11:52:29 +0500

    Update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 driver = None
 
 # random browser selection method
